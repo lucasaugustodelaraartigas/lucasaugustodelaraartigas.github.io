4c5cae1adb51c9c09b712ce96d307305800aadb8
lucasaugustodelaraartigas <lucasaugustodelaraartigas@gmail.com>
2021-10-15 02:02:07 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Sitetrainee
+# Sitetrainee
---
+++ principal.js
@@ -0,0 +1,11 @@
+var noturno = document.querySelector("#noturno");
+
+noturno.addEventListener("click", noturnoOn);
+
+function preventDefaut(event){
+    event.preventDefaut
+}
+
+function noturnoOn(){
+console.log("teste");
+}
